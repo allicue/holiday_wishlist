0f574e818aa54ae26fe34f1628c8d70462bcc452
Allison <allisonquiroz89@gmail.com>
2020-11-07 19:46:25 -0500

    fixed footer, adjusted item height, edited css styling for navbar

--- holiday_wishlist/src/components/ViewList.jsx
+++ holiday_wishlist/src/components/ViewList.jsx
@@ -10,7 +10,7 @@ function ViewList(props) {
       <Switch>
       <Route path="/viewlist">
         <h2 className="subheaderviewwishlist">WISHLIST</h2>
-        <h3 className="captionviewwishlist">If you purchased an item below, click on the item name to cross it off.</h3>
+        {/* <h3 className="captionviewwishlist">If you purchased an item below, click on the item name to cross it off.</h3> */}
 
         {props.listItems && 
           <div className="wishlistcontainer">
